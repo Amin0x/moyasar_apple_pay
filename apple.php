9335d6490824e9cd918eba944f1f410579f5b3fe
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-21 12:18:15 +0300

    Update apple.php

--- apple.php
+++ apple.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="css/main.js"></script>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="css/main.js"></script>
 </head>
 <body>
 <div class="apple-pay-button apple-pay-button-white"></div>
